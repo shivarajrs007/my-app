bdfbd5710a65de765de815cbf8981b1124ed13be
PrasadPrabhu06 <prabhuprasadjune@gmail.com>
2024-01-11 01:37:28 +0530

    updated with removing item from list

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,19 +1,31 @@
 import React from "react";
+import { useState} from "react";
 import './MainPage.css';
-const MainPage = () => {
-    const items = [{ id: 1, itemName: "item1", price: 100 }, { id: 2, itemName: "item2", price: 300 }, { id: 3, itemName: "item3", price: 400 }, { id: 4, itemName: "item4", price: 500 }];
-    const Total = items.reduce((total, item) => total + item.price, 0);
+const MainPage = () => 
+{
+    const [listItems,setListItems]=useState([{ id: 1, itemName: "item1", price: 100 }, { id: 2, itemName: "item2", price: 300 }, { id: 3, itemName: "item3", price: 400 }, { id: 4, itemName: "item4", price: 500 }]);
+    const Total = listItems.reduce((total, item) => total + item.price, 0);
+    const removeItem = (Id) => {
+        const result = listItems.filter(item => item.id != Id);
+        setListItems(result);
+        console.log(listItems)
+    }
     return (
         <div className='app-background'>
             <div className='main-container'>
                 <div className='item-list'>
-                    {items.map((item) => (
+                    {listItems.map((item) => (
                         <div className='item-container' key={item.id}>
                             <div className='item-name' >
                                 <span>{item.itemName}</span>
                             </div>
                             <div className='quantity'>
                                 <span>{item.price}</span>
+                            </div>
+                            <div className='quantity'>
+                                <button onClick={() => removeItem(item.id)}>
+                                    Delete Items
+                                </button>
                             </div>
                         </div>
                     ))}
