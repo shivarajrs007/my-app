83491aaaf7d782f837ed4aa0309d12b93fcf907b
PrasadPrabhu06 <prabhuprasadjune@gmail.com>
2024-01-10 23:25:08 +0530

    implemented ui for checkout screen

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,20 +1,24 @@
 import React from "react";
-
-const MainPage = () => 
-{
-    const items = [{ id: 1,itemName: "item1", price: "100" },{ id: 2,itemName: "item2", price: "200" },{ id: 3,itemName: "item3", price: "400" },{ id: 4,itemName: "item4", price: "500" }];
-    const arrayDataItems = items.map(course => 
-        <li className="list-group-item" key={course.id}>
-          <span>ItemName:{course.itemName}</span>
-          <span className="d-flex justify-content-end">ItemPrice:{course.price}</span>
-        </li>
-      )
+import './MainPage.css';
+const MainPage = () => {
+    const items = [{ id: 1, itemName: "item1", price: 100 }, { id: 2, itemName: "item2", price: 300 }, { id: 3, itemName: "item3", price: 400 }, { id: 4, itemName: "item4", price: 500 }];
+    const Total = items.reduce((total, item) => total + item.price, 0);
     return (
-        <div className="d-flex justify-content-center">
-            <div className="card " style={{width: '25rem'}}>
-                <ul className="list-group list-group-flush">
-                   {arrayDataItems}
-                </ul>
+        <div className='app-background'>
+            <div className='main-container'>
+                <div className='item-list'>
+                    {items.map((item) => (
+                        <div className='item-container' key={item.id}>
+                            <div className='item-name' >
+                                <span>{item.itemName}</span>
+                            </div>
+                            <div className='quantity'>
+                                <span>{item.price}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className='total'>Total: {Total}</div>
             </div>
         </div>
     );
